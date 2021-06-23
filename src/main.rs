890821b62fc3a57687b4bf80d7d969e4eea24ca9
ongaeshi <ongaeshi0621@gmail.com>
2021-06-24 00:17:40 +0900

    feat: rubyプロセスを実行する

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+
+fn main()
+{
+    Command::new("ruby")
+            .args(&["-e", "p 1, 2, 3"])
+            .status()
+            .expect("failed to execute process");
 }
